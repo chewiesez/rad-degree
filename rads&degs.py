04625891afdd5831365f574417fb114930ed01a7
Dan Foster <60412752+chewiesez@users.noreply.github.com>
2022-10-31 15:25:50 -0400

    Add files via upload

---
+++ rads&degs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 09:13:34 2022
+
+@author: fost
+"""
+
+#python coding challenge for beginners - convert radians to degrees
+ # Degree sign = u"\u00b0"
+
+def choose_func():
+    c = int(input('Enter 1 to convert RADIAN to DEGREE \nEnter 2 to convert DEGREE to RADIAN:\n'))
+    if c == 1:
+        convert_radians()
+    elif c == 2: 
+        convert_degrees()
+    else:
+        choose_func()
+
+
+def convert_radians():
+    r = float(input('Enter radians:\n'))
+    k = 57.2958
+    deg = (r*k)
+    print(str(deg) + u"\u00b0")
+
+def convert_degrees():
+    d = float(input('Enter degrees:\n'))
+    k = 180
+    r = d/k
+    print("\u03C0" + " " + str(r))
+  
+   
+choose_func()
